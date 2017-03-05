2a80902156a8634918ea9b10c7d1db9b8d72b879
AJ West <ajwest@gmail.com>
2017-03-04 23:25:16 -0500

    Tried to make a pusher, starting more menu options.

---
+++ Assets/Pusher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+
+    {
+        if (collision.gameObject.tag == "Box") { 
+
+        collision.transform.Translate(Vector3.down * Time.deltaTime * 5);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/RGBResultSelf.cs
+++ Assets/RGBResultSelf.cs
@@ -30,12 +30,30 @@ public class RGBResultSelf : MonoBehaviour {
     IEnumerator UpdateColor()
     {
 
-        yield return new WaitForSeconds(1f); //gotta wait for the stupid slider to stop sliding after ending interaction
+        yield return new WaitForSeconds(0.5f); //gotta wait for the stupid slider to stop sliding after ending interaction
         //if (SliderRed.CurrentValue != Result.material.color.r || SliderGreen.CurrentValue != Result.material.color.g || SliderBlue.CurrentValue != Result.material.color.b) {
-        Result.material.color = new Color(SliderRed.CurrentValue, SliderGreen.CurrentValue, SliderBlue.CurrentValue);
-        //}
+        float start = Time.time;
+        float elapsed = 0;
+        Color oldColor = new Color(Result.material.color.r, Result.material.color.g, Result.material.color.b);
+        Color newColor = new Color(SliderRed.CurrentValue, SliderGreen.CurrentValue, SliderBlue.CurrentValue);
+        float duration = 0.8f;
+        while (elapsed < duration)
+        {
+            // calculate how far through we are
+            elapsed = Time.time - start;
+            float normalisedTime = Mathf.Clamp(elapsed / duration, 0, 1);
+            Result.material.color = Color.Lerp(oldColor, newColor, normalisedTime);
+            // wait for the next frame
+            yield return null;
+        }
+
+         
+        
         
     }
+
+
+
 }
 
 
