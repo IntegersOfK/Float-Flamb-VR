6701cb4925b9dafa2df8d53c3dbe9e5bea1b28aa
AJ West <ajwest@gmail.com>
2017-03-05 22:46:28 -0500

    Can now toggle between sphere and cube.

--- Assets/Circleup.cs
+++ Assets/Circleup.cs
@@ -4,13 +4,58 @@ using UnityEngine;
 
 public class Circleup : MonoBehaviour {
 
+    public List<GameObject> spheres;
+    public List<GameObject> cubes;
+
 	// Use this for initialization
 	void Start () {
         //Mesh mesh = GetComponent<Mesh>();
         Mesh mesh = GetComponent<MeshFilter>().mesh;
 
     }
+
+    public void CircleUp()
+    {
+
+        foreach (GameObject cube in cubes)
+        {
+            if (cube != null)
+            {
+                cube.SetActive(false);
+            }
+        }
+
+        foreach (GameObject sphere in spheres)
+        {
+            if (sphere != null)
+            {
+                sphere.SetActive(true);
+            }
+        }
+    }
 	
+    public void SquareUp()
+    {
+
+        foreach (GameObject sphere in spheres)
+        {
+            if (sphere != null)
+            {
+                sphere.SetActive(false);
+            }
+        }
+
+        foreach (GameObject cube in cubes)
+        {
+            if (cube != null)
+            {
+                cube.SetActive(true);
+            }
+        }
+
+        
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
